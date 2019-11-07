1ac839f281a385304dfd0841e3597ad61b25f090
DE <de@pop-os.localdomain>
2019-11-08 00:19:39 +0300

    finished 42.go

---
+++ homework-4/cars/cars.go
@@ -0,0 +1,23 @@
+package cars
+
+
+type Sedan struct {
+// структура седан
+	Name       string
+	Year       int
+	Volume     int
+	IsEgnition bool
+	IsWindOpen bool
+	FreeVolume int
+}
+
+
+type Miniven struct {
+//структура минивен
+	Name       string
+	Year       int
+	Volume     int
+	IsEgnition bool
+	IsWindOpen bool
+	FreeVolume int
+}
